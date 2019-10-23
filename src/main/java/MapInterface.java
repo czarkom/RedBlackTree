2ca0b390d65f9e72b8372d27f41b9b5c1812da07
czarkom <mczarkowski@onet.pl>
2019-10-23 22:43:46 +0200

    first commit

---
+++ src/main/java/MapInterface.java
@@ -0,0 +1,4 @@
+public interface MapInterface <K extends Comparable<K>,V> {
+    public void setValue (K key, V value);
+    public V getValue(K key);
+}
---
+++ src/main/java/RBT.java
@@ -0,0 +1,13 @@
+public class RBT<K extends Comparable<K>,V> implements MapInterface<K,V>{
+
+
+    @Override
+    public void setValue(K key, V value) {
+
+    }
+
+    @Override
+    public V getValue(K key) {
+        return null;
+    }
+}
