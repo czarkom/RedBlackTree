36758e22d2c77bdcf8dfe8e2b80c9939e086fe6e
czarkom <mczarkowski@onet.pl>
2019-10-25 20:33:19 +0200

    empty tests added

---
+++ src/test/java/RBTTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+public class RBTTest {
+
+    @org.junit.Test
+    public void setValue() {
+    }
+
+    @org.junit.Test
+    public void getValue() {
+    }
+
+    @org.junit.Test
+    public void rotateLeft() {
+    }
+
+    @org.junit.Test
+    public void rotateRight() {
+    }
+}
