9736a1ed4f25421a11a29095df81f4812fde868b
czarkom <mczarkowski@onet.pl>
2019-10-26 13:29:44 +0200

    one test added

--- src/test/java/RBTTest.java
+++ src/test/java/RBTTest.java
@@ -4,6 +4,17 @@ public class RBTTest {
 
     @org.junit.Test
     public void setValue() {
+        RBT<String, Integer> tree = new RBT<>();
+        tree.setValue("a", 25);
+        tree.setValue("l", 128);
+        tree.setValue("g", 543);
+        tree.setValue("o", 37);
+        tree.setValue("r", 259);
+        tree.setValue("y", 43);
+        tree.setValue("t", 25);
+        tree.setValue("m", 20);
+
+        assertEquals(128, (int) tree.getValue("l"));
     }
 
     @org.junit.Test
