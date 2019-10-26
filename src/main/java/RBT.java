5316d0c1c890e949fa43d52dd2012616f9a2868f
czarkom <mczarkowski@onet.pl>
2019-10-26 13:21:23 +0200

    fixes in string searching

--- src/main/java/RBT.java
+++ src/main/java/RBT.java
@@ -1,8 +1,27 @@
-public class RBT<K extends Comparable<K>, V> implements MapInterface<K, V>{
+public class RBT<K extends Comparable<K>, V> implements MapInterface<K, V> {
 
 
     private static Node root = null;
 
+    public static void main(String args[]) {
+        RBT<String, Integer> tree = new RBT<>();
+        tree.setValue("a", 25);
+        tree.setValue("l", 128);
+        tree.setValue("g", 543);
+        tree.setValue("o", 37);
+        tree.setValue("r", 259);
+        tree.setValue("y", 43);
+        System.out.println(root.key);
+        tree.setValue("t", 25);
+        tree.setValue("m", 20);
+
+
+        System.out.println("Values set");
+        System.out.println(root.key);
+
+        tree.getValue("l");
+
+    }
 
     @Override
     public void setValue(K key, V value) {
@@ -18,24 +37,21 @@ public class RBT<K extends Comparable<K>, V> implements MapInterface<K, V>{
 
     @Override
     public V getValue(K key) {
-        Node<K,V> r = root;
-        boolean found = false;
-        while (r != null)
-        {
+        Node<K, V> r = root;
+        while (r != null) {
             K rval = r.key;
-            if (key.compareTo(rval) == -1)
+            if (key.compareTo(rval) < 0)
                 r = r.left;
-            else if (key.compareTo(rval) == 1)
+            else if (key.compareTo(rval) > 0)
                 r = r.right;
-            else if (key.compareTo(rval) == 0)
-            {
+            else if (key.compareTo(rval) == 0) {
                 break;
             }
         }
-        if(r==null){
+        if (r == null) {
             System.out.println("Value of given key not found");
             return null;
-        }else{
+        } else {
             System.out.println("Value of the given key:" + r.value);
         }
         return r.value;
@@ -63,14 +79,14 @@ public class RBT<K extends Comparable<K>, V> implements MapInterface<K, V>{
         return child;
     }
 
-    void swapColors(Node node1, Node node2){
+    void swapColors(Node node1, Node node2) {
         boolean temp = node1.isRed;
         node1.isRed = node2.isRed;
         node2.isRed = temp;
     }
 
-    boolean isRed(Node<K,V> node){
-        if(node == null)
+    boolean isRed(Node<K, V> node) {
+        if (node == null)
             return false;
         return node.isRed;
     }
@@ -83,45 +99,24 @@ public class RBT<K extends Comparable<K>, V> implements MapInterface<K, V>{
             myNode.left = insert(myNode.left, key, value);
         else if (key.compareTo(myNode.key) > 1)
             myNode.right = insert(myNode.right, key, value);
-        else if (key.compareTo(myNode.key) == 0){
+        else if (key.compareTo(myNode.key) == 0) {
             myNode.value = value;
             return myNode;
-        }
-        else
+        } else
             return myNode;
-        if(isRed(myNode.right) && !isRed(myNode.left)){
+        if (isRed(myNode.right) && !isRed(myNode.left)) {
             myNode = rotateLeft(myNode);
             swapColors(myNode, myNode.left);
         }
-        if(isRed(myNode.left) && isRed(myNode.left.left)){
+        if (isRed(myNode.left) && isRed(myNode.left.left)) {
             myNode = rotateRight(myNode);
             swapColors(myNode, myNode.right);
         }
-        if(isRed(myNode.left) && isRed(myNode.right)){
+        if (isRed(myNode.left) && isRed(myNode.right)) {
             myNode.isRed = !myNode.isRed;
             myNode.right.isRed = false;
             myNode.left.isRed = false;
         }
         return myNode;
-    }
-
-    public static void main(String args[]){
-        RBT<String,Integer> tree = new RBT<>();
-        tree.setValue("a",25);
-        tree.setValue("l",128);
-        tree.setValue("g",543);
-        tree.setValue("o",32);
-        tree.setValue("r",259);
-        tree.setValue("y",43);
-        System.out.println(root.key);
-        tree.setValue("t",25);
-        tree.setValue("m",20);
-
-
-        System.out.println("Values set");
-        System.out.println(root.key);
-
-        tree.getValue("o");
-
     }
 }
