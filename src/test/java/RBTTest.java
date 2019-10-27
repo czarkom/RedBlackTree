422a189761cda19b48d2c1cc20eb3b91b3061f68
czarkom <mczarkowski@onet.pl>
2019-10-27 20:36:24 +0100

    new tests added

--- src/test/java/RBTTest.java
+++ src/test/java/RBTTest.java
@@ -1,10 +1,12 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class RBTTest {
+    RBT<String, Integer> tree = new RBT<>();
 
     @org.junit.Test
-    public void setValue() {
-        RBT<String, Integer> tree = new RBT<>();
+    public void isValueFromGivenKeyCheckedCorrectly() {
         tree.setValue("a", 25);
         tree.setValue("l", 128);
         tree.setValue("g", 543);
@@ -13,19 +15,30 @@ public class RBTTest {
         tree.setValue("y", 43);
         tree.setValue("t", 25);
         tree.setValue("m", 20);
-
         assertEquals(128, (int) tree.getValue("l"));
     }
 
     @org.junit.Test
-    public void getValue() {
+    public void isRootCorrect() {
+        tree.setValue("a", 25);
+        tree.setValue("l", 128);
+        tree.setValue("g", 543);
+        tree.setValue("o", 37);
+        tree.setValue("r", 259);
+        tree.setValue("y", 43);
+        tree.setValue("t", 25);
+        tree.setValue("m", 20);
+        assertEquals("o", tree.root.key);
     }
 
-    @org.junit.Test
-    public void rotateLeft() {
+    @Test(expected = IllegalArgumentException.class)
+    public void isNullNodeKeyExceptionThrownCorrectly() {
+        RBT<String, Integer> tree = new RBT<>();
+        tree.setValue(null, 234);
     }
 
-    @org.junit.Test
-    public void rotateRight() {
+    @Test(expected = IllegalStateException.class)
+    public void isNullRootExceptionThrownCorrectly() {
+        tree.getValue("o");
     }
 }
